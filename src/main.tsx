8811e0bf326d869a39aad7c2fcc927301a1303cf
bigstth~ <sitthidsn@gmail.com>
2024-01-21 17:55:19 +0700

    ⚡️ Added speed insight

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { ThemeProvider } from '@/ThemeProvider.tsx'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ThemeProvider } from "@/ThemeProvider.tsx";
+import App from "./App.tsx";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
-import './index.css'
+import "./index.css";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
   },
-])
+]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <RouterProvider router={router} />
+      <SpeedInsights />
     </ThemeProvider>
   </React.StrictMode>,
-)
+);
